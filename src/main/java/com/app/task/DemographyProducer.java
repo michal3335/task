088f3fb00c5b48147c1a03296c314bce1ee51c3d
michal3335 <michal3335@gmail.com>
2021-08-15 19:04:57 +0200

    Final Version

--- src/main/java/com/app/task/DemographyProducer.java
+++ src/main/java/com/app/task/DemographyProducer.java
@@ -5,7 +5,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
 @Component
 public class DemographyProducer {
